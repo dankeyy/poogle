3c00c2d246e88ce043089935f929d49e00e7ff6d
dankeyy <dankeyy@protonmail.com>
2022-06-01 18:18:59 +0300

    messing with type parsing, might change approach later

--- poogle.py
+++ poogle.py
@@ -3,8 +3,14 @@ import ast
 def main():
     test = "(x: int, y: str) -> str"
     query = "def f" + test + ":..."
-    print(ast.dump(ast.parse(query), indent=4))
+    parsed = ast.parse(query)
+    # print(ast.dump(parsed, indent=4))
+    func = parsed.body[0]
 
+    arg_types = tuple(x.annotation.id for x in func.args.args)
+    return_type = func.returns.id
+
+    print(arg_types, return_type)
 
 if __name__ == '__main__':
     main()
