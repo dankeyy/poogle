9659cc6f1adf5d883454a4e49aba60cfce7b1c66
dankeyy <dankeyy@protonmail.com>
2022-09-03 17:38:58 +0300

    refactor

--- poogle.py
+++ poogle.py
@@ -3,6 +3,7 @@
 import sys
 import difflib
 import webbrowser
+import functools
 
 PYTHON_DOCS = "https://docs.python.org/3/"
 STDLIB_DOCS = "library/"
@@ -24,6 +25,21 @@ def matches(query, pool, default=None):
     return matched.pop() if matched else default
 
 
+def parse(head: str, tail: str, possibilities: list, url: str, fallback: str):
+    if tail:
+        method = matches(tail, possibilities, default=tail)
+        return url + head + '.' + method
+    else:
+        return fallback
+
+
+def a_type(t):
+    """checks if a string represents a built in type, if so, returns the type"""
+    builtin = vars(__builtins__).get(t) # maybe None
+    # None is not a built in type (NoneType is) so it will fail the following isinstance check
+    if isinstance(builtin, type):
+        return builtin
+
 
 def docc(query: str):
     """if a match was found, jumps to the the docs pinned on the match,
@@ -31,68 +47,59 @@ def docc(query: str):
     Note that if the module's name if either are slightly incorrect,
     you probably will still be redirected to the correct page and tag."""
 
-    base = ["iterator", "container", "generator", "agen", "object"] + dir(__builtins__)
+    base = ["iterator", "container", "generator", "agen", "object", "class", "frame"] + dir(__builtins__)
     modules = sys.stdlib_module_names
     head, _, tail = query.partition('.')
-    path = ""
+    path = PYTHON_DOCS
 
+    # some form of a builtin
     if res := matches(head, base):
-        if res == "object":
-            if tail:
-                possibilities = dir(object)
-                method = matches(tail, possibilities, tail)
-                path = DATA_MODEL_DOCS + res + '.' + method
-            else:
-                path = DATA_MODEL_MAIN_DOCS
+        parse_unique = functools.partial(parse, head=res, tail=tail)
 
-        elif isinstance(builtin := vars(__builtins__).get(res), type) or res in ("iterator", "container"):
-            if tail:
-                possibilities = dir(builtin) if builtin else ("__iter__", "__next__")
-                method = matches(tail, possibilities, default=tail)
-                path = TYPE_DOCS + res + '.' + method
+        # most likely an implementable dunder method shit
+        if res in ("object", "class", "frame"):
+            return PYTHON_DOCS + parse_unique(
+                possibilities=dir(object),
+                url=DATA_MODEL_DOCS,
+                fallback=DATA_MODEL_MAIN_DOCS
+            )
 
-            else: # no tail, looking for just a built in type (e.g frozenset)
-                path = TYPE_DOCS + res
+        # it's a type
+        if builtin := a_type(res) or res in ("iterator", "container"):
+            return PYTHON_DOCS + parse_unique(
+                possibilities = dir(builtin) if builtin else ["__iter__", "__next__"],
+                url=TYPE_DOCS,
+                fallback=TYPE_DOCS + res
+            )
 
+        # generator or async generator coroutine
+        if res in ("generator", "agen"):
+            return PYTHON_DOCS + parse_unique(
+                possibilities=dir(AGEN) if res == "agen" else dir(GEN),
+                url=GEN_DOCS,
+                fallback=GEN_MAIN_DOCS
+            )
 
-        elif res  == "generator":
-            if tail:
-                possibilities = dir(GEN)
-                method = matches(tail, possibilities, default=tail)
-                path = GEN_DOCS + res + '.' + method
-            else:
-                path = GEN_MAIN_DOCS
+        # just a builtin function
+        return PYTHON_DOCS + FUNCTION_DOCS + res
 
-        elif res == "agen":
-            if tail:
-                possibilities = dir(AGEN)
-                method = matches(tail, possibilities, default=tail)
-                path = GEN_DOCS + res + '.' + method
-            else:
-                path = GEN_MAIN_DOCS
+    # stdlib module
+    if module := matches(head, modules):
+        return PYTHON_DOCS + parse(
+            head=module,
+            tail=tail,
+            possibilities=dir(__import__(module)),
+            url=STDLIB_DOCS + module + "#",
+            fallback=STDLIB_DOCS + module
+        )
 
+    # couldn't find anything
+    return PYTHON_DOCS + "search.html?q=" + query
 
-        else:
-            path = FUNCTION_DOCS + res
-
-    elif res := matches(head, modules):
-        path = STDLIB_DOCS + res
-        if tail:
-            function = matches(tail, dir(__import__(res)), default='')
-            path = STDLIB_DOCS + res + "#" + res + '.' + function
-
-
-    # couldn't find anything, just search
-    if not path:
-        path = "search.html?q=" + query
-
-    return PYTHON_DOCS + path
-
-# path = "library/functions#" + res if not tail else "library/stdtypes#" + query
-# path = "library/" + res if not tail else "library/" + res + "#" + res + '.' + matches(tail, dir(__import__(res)), default='')
 
 def visit(query):
     url = docc(query)
+    print("Opening", url, "in broswer")
     webbrowser.open(url)
 
 
